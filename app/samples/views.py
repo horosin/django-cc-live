c0d07e2bda5e046f04bb6f14815474f34151cffd
Karol Horosin <karol.horosin@gmail.com>
2019-05-07 11:14:38 +0100

    Index and other page

--- app/samples/views.py
+++ app/samples/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Index page')
+
+
+def other_page(request):
+   context = {
+       'test': 'passing a value to the template'
+   }
+   return render(request, 'samples/other.html', context)
+
