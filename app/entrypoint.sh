e447b529577a0ef73973e09d8520fded7dd8906c
Karol <karol@Karols-MacBook-Pro.local>
2019-05-13 22:35:35 +0100

    ENtrypoint to wait for postgres

---
+++ app/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+until curl http://${DATABASE_URL:-db}:${DATABASE_PORT:-5432}/ 2>&1 | grep '52'; do
+  echo "Postgres is unavailable - sleeping"
+  sleep 2
+done
+
+echo "Postgres is up - continuing"
+
+if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
+    /venv/bin/python manage.py migrate --noinput
+fi
+
+if [ "x$DJANGO_MANAGEPY_COLLECTSTATIC" = 'xon' ]; then
+    /venv/bin/python manage.py collectstatic --noinput
+fi
+
+exec "$@"
