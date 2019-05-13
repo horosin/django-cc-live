aa5907f2001059d8ea67342082efcf52bc882c87
Karol <karol@Karols-MacBook-Pro.local>
2019-05-13 23:47:05 +0100

    Protect views with permissions

--- app/samples/views.py
+++ app/samples/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.views import generic
+from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.mixins import PermissionRequiredMixin
+
 
 from . import models
 
@@ -11,6 +14,7 @@ def index(request):
     return HttpResponse('Index page')
 
 
+@permission_required('samples.view_sample')
 def other_page(request):
     first_sample = models.Sample.objects.first()
     context = {
@@ -22,7 +26,9 @@ def other_page(request):
 class SampleListView(generic.ListView):
     model = models.Sample
 
-class SampleCreateView(generic.CreateView):
-   model = models.Sample
-   fields = '__all__'
-   success_url = reverse_lazy('list')
+
+class SampleCreateView(PermissionRequiredMixin, generic.CreateView):
+    model = models.Sample
+    fields = '__all__'
+    success_url = reverse_lazy('list')
+    permission_required = 'samples.create_sample'
